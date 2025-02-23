d1af98172b86a7e128cca0a76b943d532aad76c7
linkupx <linkup.x.corp@gmail.com>
2025-02-23 20:29:46 +0900

    feat: PWA 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,15 @@ import withPWA from 'next-pwa';
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 const config = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
 })(nextConfig);
 
 export default config;
